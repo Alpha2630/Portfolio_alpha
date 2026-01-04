fd4330ceb69eef3490c7860f89d257c98fc3c33e
Alking <alrey2635@gmail.com>
2026-01-04 16:27:06 +0300

    initial commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+function App() {
+
+
+  return (
+    <div className='p-5 md:px-[15%]'>
+      <Navbar/>
+      <Home/>
+    </div>
+
+  )
+}
+
+export default App
